03d9193a92a32b3102d437d882b88de68ca7cc6e
cahya bagus sanjaya <9c96b6@gmail.com>
2015-02-06 04:24:18 +0700

    ganti metode
    ganti metode

--- cekVersi/cekVersi.dpr
+++ cekVersi/cekVersi.dpr
@@ -1,87 +1,60 @@
 program cekVersi;
 
 uses
-  Classes,
-  WinInet,
-  ShellAPI,
   Windows,
   Dialogs,
-  Messages,
-  Controls,
-  SysUtils,
   Variants,
-  uLkJSON;
+  uLkJSON,
+  ExtActns;
 
-function loadVersion(const Url: string): string;
-var
-  NetHandle: HINTERNET;
-  UrlHandle: HINTERNET;
-  Buffer: array[0..1024] of Char;
-  BytesRead: dWord;
+function Download_HTM( const sURL, sLocalFileName : string): boolean;
 begin
-  //cek koneksi
-  Result := '';
-
-  if not(InternetGetConnectedState(nil,0))then
-  begin
-    Exit;
-  end;
-
-  NetHandle := InternetOpen('Delphi 5.x', INTERNET_OPEN_TYPE_PRECONFIG, nil, nil, 0);
-
-  if Assigned(NetHandle) then 
-  begin
-    UrlHandle := InternetOpenUrl(NetHandle, PChar(Url), nil, 0, INTERNET_FLAG_RELOAD, 0);
-
-    if Assigned(UrlHandle) then
-      { UrlHandle valid? Proceed with download }
-    begin
-      FillChar(Buffer, SizeOf(Buffer), 0);
-      repeat
-        Result := Result + Buffer;
-        FillChar(Buffer, SizeOf(Buffer), 0);
-        InternetReadFile(UrlHandle, @Buffer, SizeOf(Buffer), BytesRead);
-      until BytesRead = 0;
-      InternetCloseHandle(UrlHandle);
-    end
-    else
-      { UrlHandle is not valid. Raise an exception. }
-      raise Exception.CreateFmt('Cannot open URL %s', [Url]);
-
-    InternetCloseHandle(NetHandle);
-  end
-  else
-    { NetHandle is not valid. Raise an exception }
-    raise Exception.Create('Unable to initialize Wininet');
+  Result := True;
+   with TDownLoadURL.Create( nil) do
+   try
+    URL:=sURL;
+    Filename:=sLocalFileName;
+     try
+      ExecuteTarget( nil) ;
+     except
+      Result:=False
+     end;
+   finally
+    Free;
+   end;
 end;
 
 //param 1 -> versi
 //param 2 -> applikasi
 
-var jsonApp,versi,download: string;
+var versi,download: string;
     js: TlkJsonObject;
 begin
-  jsonApp:= loadVersion('http://gain-profit.github.io/profit.json');
-  js := TlkJSON.ParseText(jsonApp) as TlkJSONobject;
+  if NOT Download_HTM('http://gain-profit.github.io/profit.json','profit.json') then
+  begin
+    ShowMessage('Error in HTML file download') ;
+    Exit;
+  end;
 
-  versi     := VarToStr(js.Field['app'].Field['gudang'].Field['versi'].Value);
-  download  := VarToStr(js.Field['app'].Field['gudang'].Field['download'].Value);
+  try
+    js := TlkJSONstreamed.loadfromfile('profit.json') as TlkJsonObject;
+    versi     := VarToStr(js.Field['app'].Field[ParamStr(2)].Field['versi'].Value);
+    download  := VarToStr(js.Field['app'].Field[ParamStr(2)].Field['download'].Value);
+  except
+    exit;
+  end;
 
   if versi <>''then
   begin
     if paramStr(1)<> versi then
     begin
-      if MessageDlg('Aplikasi '+ParamStr(2)+' Terbaru Telah keluar:' + #13#10 +
-        'Versi terbaru : ' + versi + #13#10#13#10 +
-        'Download Applikasi Terbaru?',  mtWarning, [mbYes, mbNo], 0) = mrYes
-        then
-      begin
-        WinExec(PChar('rundll32 url.dll,FileProtocolHandler '+ download),
-        SW_MAXIMIZE);
-      end;
+      ShowMessage('Aplikasi '+ParamStr(2)+' Terbaru Telah keluar:' + #13#10 +
+      'Versi terbaru : ' + versi + #13#10#13#10 +
+      'Download Applikasi Terbaru!!!' );
+
+      WinExec(PChar('rundll32 url.dll,FileProtocolHandler '+ download),
+      SW_MAXIMIZE);
     end;
   end;
-  
-
 end.
  
