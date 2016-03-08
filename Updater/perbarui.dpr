09fdefc561f7df224646f19b7fe8c5de6db57c4c
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-08 16:56:54 +0700

    single instance app
    hanya boleh berjalan satu instance.

--- Updater/perbarui.dpr
+++ Updater/perbarui.dpr
@@ -2,18 +2,25 @@ program perbarui;
 
 uses
   Forms,
+  // Unit yang digunakan untuk cek instance sebelumnya.
+  CheckPrevious In 'CheckPrevious.pas',
   UnitUtama in 'UnitUtama.pas' {FormUtama},
   UDM in 'UDM.pas' {dm: TDataModule};
 
 {$R *.res}
+// agar aplikasi berjalan sebagai administrator
 {$R 'RequestAdmin.res'}
 
 begin
-  Application.Initialize;
-  Application.Title := 'Gain Profit Updater';
-  Application.CreateForm(TFormUtama, FormUtama);
-  Application.CreateForm(Tdm, dm);
-  // Jangan Tampilkan Form Utama ketika di create
-  Application.ShowMainForm:=False;
-  Application.Run;
+  // Hanya boleh ada satu instance
+  if not CheckPrevious.RestoreIfRunning(Application.Handle, 1) then
+  begin
+    Application.Initialize;
+    Application.Title := 'Gain Profit Updater';
+    Application.CreateForm(TFormUtama, FormUtama);
+    Application.CreateForm(Tdm, dm);
+    // Jangan Tampilkan Form Utama ketika di create
+    Application.ShowMainForm:=False;
+    Application.Run;
+  end;
 end.
