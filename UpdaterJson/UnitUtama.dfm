ee63d02c69030972b346b9417f22ba80b5fdc768
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-14 20:27:53 +0700

    json updater
    untuk update file updater.json di Gain-Profit.github.io

---
+++ UpdaterJson/UnitUtama.dfm
@@ -0,0 +1,124 @@
+object FormUtama: TFormUtama
+  Left = 81
+  Top = 151
+  BorderIcons = [biSystemMenu]
+  BorderStyle = bsSingle
+  Caption = 'Gain Profit Updater'
+  ClientHeight = 442
+  ClientWidth = 912
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  KeyPreview = True
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object cxGrid1: TcxGrid
+    Left = 0
+    Top = 41
+    Width = 912
+    Height = 382
+    Align = alClient
+    TabOrder = 0
+    object TableView: TcxGridDBTableView
+      NavigatorButtons.ConfirmDelete = False
+      DataController.Summary.DefaultGroupSummaryItems = <>
+      DataController.Summary.FooterSummaryItems = <>
+      DataController.Summary.SummaryGroups = <>
+      OptionsView.ColumnAutoWidth = True
+      OptionsView.GroupByBox = False
+      object TableViewColumn1: TcxGridDBColumn
+        Caption = 'Nama'
+        DataBinding.ValueType = 'String'
+        Options.Editing = False
+        Width = 137
+      end
+      object TableViewColumn2: TcxGridDBColumn
+        Caption = 'Path'
+        DataBinding.ValueType = 'String'
+        Options.Editing = False
+        Width = 75
+      end
+      object TableViewColumn3: TcxGridDBColumn
+        Caption = 'MD5'
+        DataBinding.ValueType = 'String'
+        Options.Editing = False
+        Width = 129
+      end
+      object TableViewColumn4: TcxGridDBColumn
+        Caption = 'Versi'
+        DataBinding.ValueType = 'String'
+        Options.Editing = False
+        Width = 65
+      end
+      object TableViewColumn5: TcxGridDBColumn
+        Caption = 'Download'
+        DataBinding.ValueType = 'String'
+        Options.Editing = False
+        Width = 278
+      end
+    end
+    object Level: TcxGridLevel
+      GridView = TableView
+    end
+  end
+  object pnl1: TPanel
+    Left = 0
+    Top = 0
+    Width = 912
+    Height = 41
+    Align = alTop
+    TabOrder = 1
+    object btnBrowse: TButton
+      Left = 8
+      Top = 8
+      Width = 75
+      Height = 21
+      Caption = 'Pilih Folder'
+      TabOrder = 0
+      OnClick = btnBrowseClick
+    end
+    object edtFolder: TEdit
+      Left = 88
+      Top = 8
+      Width = 633
+      Height = 21
+      TabOrder = 1
+      Text = 'D:\WORKSPACES\GAIN PROFIT\GAIN PROFIT'
+    end
+    object btnCek: TButton
+      Left = 728
+      Top = 8
+      Width = 89
+      Height = 21
+      Caption = 'Cek'
+      TabOrder = 2
+      OnClick = btnCekClick
+    end
+    object btnSimpan: TButton
+      Left = 824
+      Top = 8
+      Width = 81
+      Height = 21
+      Caption = 'Simpan'
+      TabOrder = 3
+      OnClick = btnSimpanClick
+    end
+  end
+  object status: TStatusBar
+    Left = 0
+    Top = 423
+    Width = 912
+    Height = 19
+    Panels = <
+      item
+        Text = 'Status'
+        Width = 50
+      end>
+  end
+end
---
+++ UpdaterJson/UnitUtama.pas
@@ -0,0 +1,250 @@
+unit UnitUtama;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, cxStyles, cxCustomData, cxGraphics, cxFilter, cxData,
+  cxDataStorage, cxEdit, DB, cxDBData, cxGridCustomTableView, cxGridTableView,
+  cxGridDBTableView, cxGridLevel, cxClasses, cxControls, cxGridCustomView,
+  cxGrid, ExtCtrls, IdHTTP, uLkJSON, ExtActns, ComCtrls, frxBarcode, frxClass,
+  AbBase, AbBrowse, AbZBrows, AbUnzper, TlHelp32, AbComCtrls, AbArcTyp, ShellAPI,
+  FileCtrl, IdHashMessageDigest, idHash;
+
+type
+  TFormUtama = class(TForm)
+    TableView: TcxGridDBTableView;
+    Level: TcxGridLevel;
+    cxGrid1: TcxGrid;
+    TableViewColumn1: TcxGridDBColumn;
+    TableViewColumn2: TcxGridDBColumn;
+    TableViewColumn3: TcxGridDBColumn;
+    pnl1: TPanel;
+    TableViewColumn4: TcxGridDBColumn;
+    TableViewColumn5: TcxGridDBColumn;
+    status: TStatusBar;
+    btnBrowse: TButton;
+    edtFolder: TEdit;
+    btnCek: TButton;
+    btnSimpan: TButton;
+    function _get(baris, kolom: Integer): string;
+    procedure _set(baris, kolom: Integer; _isi: variant);
+    procedure FormCreate(Sender: TObject);
+    procedure btnBrowseClick(Sender: TObject);
+    procedure btnCekClick(Sender: TObject);
+    procedure btnSimpanClick(Sender: TObject);
+  private
+  public
+    thisPath: string;
+  end;
+
+var
+  FormUtama: TFormUtama;
+  Json: string;
+
+implementation
+
+{$R *.dfm}
+
+function GetInfoApp(const FileName: string; jenis: string): string;
+type
+  PLandCodepage = ^TLandCodepage;
+
+  TLandCodepage = record
+    wLanguage, wCodePage: word;
+  end;
+var
+  dummy, len: cardinal;
+  buf, pntr: pointer;
+  lang: string;
+begin
+  len := GetFileVersionInfoSize(PChar(FileName), dummy);
+  if len = 0 then
+    RaiseLastOSError;
+  GetMem(buf, len);
+  try
+    if not GetFileVersionInfo(PChar(FileName), 0, len, buf) then
+      RaiseLastOSError;
+
+    if not VerQueryValue(buf, '\VarFileInfo\Translation\', pntr, len) then
+      RaiseLastOSError;
+
+    lang := Format('%.4x%.4x', [PLandCodepage(pntr)^.wLanguage, PLandCodepage(pntr)
+      ^.wCodePage]);
+
+    if VerQueryValue(buf, PChar('\StringFileInfo\' + lang + jenis), pntr, len) then
+      result := PChar(pntr);
+  finally
+    FreeMem(buf);
+  end;
+end;
+
+procedure ListFileDir(Path, extensi: string; FileList: TStrings);
+var
+  SR: TSearchRec;
+begin
+  if FindFirst(Path + '\*.*', faAnyFile, SR) = 0 then
+  begin
+    repeat
+      if (SR.Attr <> faDirectory) then
+      begin
+        if ExtractFileExt(SR.Name) = extensi then
+        begin
+          FileList.Add(Path + '\' + SR.Name);
+        end;
+      end
+      else
+      begin
+        if not (SR.Name = '.') and not (SR.Name = '..') then
+        begin
+          ListFileDir(Path + '\' + SR.Name, extensi, FileList);
+        end;
+      end;
+    until FindNext(SR) <> 0;
+    FindClose(SR);
+  end;
+end;
+
+//returns MD5 has for a file
+function MD5(const fileName: string): string;
+var
+  idmd5: TIdHashMessageDigest5;
+  fs: TFileStream;
+begin
+  idmd5 := TIdHashMessageDigest5.Create;
+  fs := TFileStream.Create(fileName, fmOpenRead or fmShareDenyWrite);
+  try
+    result := idmd5.AsHex(idmd5.HashValue(fs));
+  finally
+    fs.Free;
+    idmd5.Free;
+  end;
+end;
+
+function program_versi(exeName: string): string;
+var
+  V1, V2, V3, V4: Word;
+  VerInfoSize, VerValueSize, Dummy: DWORD;
+  VerInfo: Pointer;
+  VerValue: PVSFixedFileInfo;
+begin
+  VerInfoSize := GetFileVersionInfoSize(PChar(exeName), Dummy);
+  GetMem(VerInfo, VerInfoSize);
+  GetFileVersionInfo(PChar(exeName), 0, VerInfoSize, VerInfo);
+  VerQueryValue(VerInfo, '\', Pointer(VerValue), VerValueSize);
+  with VerValue^ do
+  begin
+    V1 := dwFileVersionMS shr 16;
+    V2 := dwFileVersionMS and $FFFF;
+    V3 := dwFileVersionLS shr 16;
+    V4 := dwFileVersionLS and $FFFF;
+  end;
+  FreeMem(VerInfo, VerInfoSize);
+
+  Result := IntToStr(V1) + '.' + IntToStr(V2) + '.' + IntToStr(V3) + '.' + IntToStr(V4);
+end;
+
+procedure TFormUtama._set(baris, kolom: Integer; _isi: variant);
+begin
+  TableView.DataController.SetValue(baris, kolom, _isi);
+end;
+
+function TFormUtama._get(baris, kolom: Integer): string;
+begin
+  Result := TableView.DataController.getValue(baris, kolom);
+end;
+
+procedure TFormUtama.FormCreate(Sender: TObject);
+var
+  tempat: string;
+begin
+  tempat := edtFolder.Text;
+  ThisPath := StringReplace(tempat, '\', '/', [rfReplaceAll]);
+end;
+
+procedure TFormUtama.btnBrowseClick(Sender: TObject);
+var
+  chosenDirectory: string;
+begin
+  if selectdirectory('Select a directory', '', chosenDirectory) then
+    edtFolder.Text := chosenDirectory;
+end;
+
+procedure TFormUtama.btnCekClick(Sender: TObject);
+var
+  listFile: TStringList;
+  NoItem: Integer;
+  companyName, internalName: string;
+  fullNama, namaSaja, nama, path, md5file, versi, download: string;
+begin
+  listFile := TStringList.Create;
+
+  ListFileDir(edtFolder.Text, '.exe', ListFile);
+
+  TableView.DataController.RecordCount := listFile.Count;
+  for NoItem := 0 to listFile.Count - 1 do
+  begin
+    fullNama := listFile.Strings[NoItem];
+    nama := ExtractFileName(fullNama);
+    namaSaja := Copy(nama, 0, Length(nama) - 3);
+    path := Copy(fullNama, Length(edtFolder.Text) + 1, Length(fullNama) - Length
+      (edtFolder.Text) - Length(nama));
+    md5file := MD5(fullNama);
+    versi := program_versi(fullNama);
+    // ambil info untuk company name
+    companyName := GetInfoApp(fullNama, '\CompanyName');
+    // ambil info untuk Internal name
+    internalName := GetInfoApp(fullNama, '\InternalName');
+    download := 'https://github.com/' + companyName + '/' + internalName +
+      '/releases/download/v' + versi + '/' + namaSaja + '-' + versi + '.zip';
+
+    _set(NoItem, 0, nama);
+    _set(NoItem, 1, StringReplace(path, '\', '/', [rfReplaceAll]));
+    _set(NoItem, 2, md5file);
+    _set(NoItem, 3, versi);
+    _set(NoItem, 4, download);
+  end;
+  TableView.ApplyBestFit();
+end;
+
+procedure TFormUtama.btnSimpanClick(Sender: TObject);
+var
+  X: TextFile;
+  js, jsonDetail: TlkJSONobject;
+  jsonList: TlkJSONlist;
+  no, i: Integer;
+  json: string;
+begin
+  try
+    js := TlkJSONobject.Create(False);
+    jsonList := TlkJSONlist.Create;
+
+    for no := 0 to TableView.DataController.RecordCount - 1 do
+    begin
+      jsonDetail := TlkJSONobject.Create(False);
+      jsondetail.Add('nama', _get(no, 0));
+      jsondetail.Add('path', _get(no, 1));
+      jsondetail.Add('md5', _get(no, 2));
+      jsondetail.Add('versi', _get(no, 3));
+      jsondetail.Add('download', _get(no, 4));
+      jsonList.Add(jsonDetail);
+    end;
+    js.Add('profit', jsonList);
+
+    i := 0;
+    json := GenerateReadableText(js, i);
+
+    assignfile(X, 'updater.json');
+    rewrite(X);
+    write(X, json);
+    closefile(X);
+  except
+      on E: Exception do
+      begin
+        showmessage('SIMPAN DATA GAGAL...'#10#13'' + E.message);
+      end;
+  end;
+end;
+
+end.
+
