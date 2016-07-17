677c3fe782539d81a920233dbde6fc19c55647a6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-07-17 14:39:16 +0700

    cek app version.

--- UpdaterService/Utama.pas
+++ UpdaterService/Utama.pas
@@ -65,6 +65,29 @@ begin
     Result := '';
 end;
 
+function program_versi(exeName: string): string;
+var
+  V1, V2, V3, V4: Word;
+  VerInfoSize, VerValueSize, Dummy: DWORD;
+  VerInfo: Pointer;
+  VerValue: PVSFixedFileInfo;
+begin
+  VerInfoSize := GetFileVersionInfoSize(PChar(exeName), Dummy);
+  GetMem(VerInfo, VerInfoSize);
+  GetFileVersionInfo(PChar(exeName), 0, VerInfoSize, VerInfo);
+  VerQueryValue(VerInfo, '\', Pointer(VerValue), VerValueSize);
+  with VerValue^ do
+  begin
+    V1 := dwFileVersionMS shr 16;
+    V2 := dwFileVersionMS and $FFFF;
+    V3 := dwFileVersionLS shr 16;
+    V4 := dwFileVersionLS and $FFFF;
+  end;
+  FreeMem(VerInfo, VerInfoSize);
+
+  Result := IntToStr(V1) + '.' + IntToStr(V2) + '.' + IntToStr(V3) + '.' + IntToStr(V4);
+end;
+
 procedure TGainUpdater.Log(msg: string);
 var
   X: TextFile;
