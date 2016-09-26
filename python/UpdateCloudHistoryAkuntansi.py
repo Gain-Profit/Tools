3bc0694c98a055150dab153eedf2246ed803eb1b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-09-26 20:15:08 +0700

    hanya (connect dan kirim data) ke server jika ada data yang diubah.

--- python/UpdateCloudHistoryAkuntansi.py
+++ python/UpdateCloudHistoryAkuntansi.py
@@ -3,16 +3,13 @@ import mysql.connector
 import os
 from collections import deque
 
-cns = mysql.connector.connect(user='root', password='server', database='akuntan',host='localhost')
-cus = cns.cursor()
-
 cnx = mysql.connector.connect(user='root', password='server', database='profit',host='localhost')
 cux = cnx.cursor()
 
 query_history = ("SELECT kd_perusahaan, kd_kiraan, tahun, bulan, saldo_awal, debet, kredit "
               "FROM tb_jurnal_history WHERE sync = 0")
 cux.execute(query_history,)
-print(cux)
+# print(cux)
 
 d = deque()
 for (data) in cux:
@@ -22,21 +19,26 @@ for (data) in cux:
 
 cux.close()
 
-replace_query = ("REPLACE INTO jurnal_sejarahs "
-                 "(perusahaan_id, kiraan_id, tahun, bulan, saldo_awal, debit, kredit) "
-                 "VALUES (%s, %s, %s, %s, %s, %s, %s)")
-                
-cus.executemany(replace_query,d)
-cns.commit()
+if d:
+    print(d)
+    cns = mysql.connector.connect(user='root', password='server', database='akuntan',host='localhost')
+    cus = cns.cursor()
 
-cus.close()
+    replace_query = ("REPLACE INTO jurnal_sejarahs "
+                    "(perusahaan_id, kiraan_id, tahun, bulan, saldo_awal, debit, kredit) "
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+                    
+    cus.executemany(replace_query,d)
+    cns.commit()
 
-cbx = cnx.cursor()
-update_query = ("UPDATE tb_jurnal_history SET sync = 1 WHERE sync = 0")
-                
-cbx.execute(update_query,)
-cnx.commit()
+    cus.close()
+    cns.close()
 
+    cbx = cnx.cursor()
+    update_query = ("UPDATE tb_jurnal_history SET sync = 1 WHERE sync = 0")
+                    
+    cbx.execute(update_query,)
+    cnx.commit()
+    
 cnx.close()
-cns.close()
 
